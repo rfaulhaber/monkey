d9e0bdf63cb819cdff3df470b791fb19f3a33d49
Ryan Faulhaber <ryf@sent.as>
2022-11-09 17:41:35 -0500

    messing with flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, crane }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.lib.${system};
+        projectName = "monkey";
       in {
-        devShell = pkgs.mkShell {
+        packages.${projectName} =
+          craneLib.buildPackage { src = craneLib.cleanCargoSource ./.; };
+
+        packages.default = self.packages.${system}.${projectName};
+
+        apps.${projectName} = {
+          type = "app";
+          program = "${self.packages."${system}".default}/bin/monkey";
+        };
+
+        apps.default = self.apps.${system}.monkey;
+
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
             rustc
@@ -17,6 +36,7 @@
             clippy
             rust-analyzer
             rustup
+            crate2nix
           ];
         };
       });
